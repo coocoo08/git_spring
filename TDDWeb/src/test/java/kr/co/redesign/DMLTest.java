169f8d9c9998578bdfb599f34318f5c5b98b442f
Cat Hanbit <futurely08@naver.com>
2022-10-28 12:24:39 +0900

    update 추가

--- TDDWeb/src/test/java/kr/co/redesign/DMLTest.java
+++ TDDWeb/src/test/java/kr/co/redesign/DMLTest.java
@@ -30,7 +30,7 @@ public class DMLTest {
 		assertTrue(conn != null);
 	}
 	
-	@Test
+	//@Test
 	public void insertUserTest() throws SQLException {
 		User user = new User("ezen", "0111", "ezen student", "ezen@gmail.com", new Date(), "facebook", new Date());
 		deleteAll();
@@ -70,7 +70,7 @@ public class DMLTest {
 		return rowCnt;
 	}
 	
-	@Test
+	//@Test
 	public void selectUserTest() throws SQLException {
 		deleteAll();
 		User user = new User("ezen", "0111", "ezen student", "ezen@gmail.com", new Date(), "facebook", new Date());
@@ -102,6 +102,81 @@ public class DMLTest {
 			user.setReg_date(new Date(rs.getTimestamp(7).getTime()));
 		}
 		return null;
+	}
+	
+	@Test
+	public void deleteUserTest() throws SQLException {
+		deleteAll();
+		int rowCnt = deleteUser("ezen4");
+		assertTrue(rowCnt == 0);
 		
+		User user = new User("ezen", "0111", "ezen2", "ezen@gmail.com", new Date(), "facebook", new Date());
+		rowCnt = insertUser(user);
+		assertTrue(rowCnt == 1);
+		
+		deleteUser(user.getId());
+		assertTrue(rowCnt == 1);
+		
+		assertTrue(selectUser(user.getId())==null);
+	}
+	
+	public int deleteUser(String id) throws SQLException {
+		Connection conn = ds.getConnection();
+		
+		String sql = "delete from t_user where id = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		
+		pstmt.setString(1, id);
+		
+//		int rowCnt= pstmt.executeUpdate();
+//		return rowCnt;
+		return pstmt.executeUpdate();
+	}
+	
+	@Test
+	public void updateUserTest() throws SQLException {
+		deleteAll();
+		User user = new User("ezen5", "0111", "ezen2", "ezen@gmail.com", new Date(), "facebook", new Date());
+		int rowCnt = insertUser(user);
+		assertTrue(rowCnt == 1);
+
+		user.setPwd("0112");
+		user.setEmail("ezen@gmail.com");
+		rowCnt = updateUser(user);
+		assertTrue(rowCnt == 1);
+		
+//		User user2 = selectUser(user.getId());
+//		System.out.println("user = " + user);
+//		System.out.println("user = " + user2);
+//		assertTrue(user.equals(user2));
+	}
+	
+	// 매개변수로 받은 사용자 정보로 t_user 테이블을 update하는 메서드
+	public int updateUser(User user) throws SQLException {
+		Connection conn = ds.getConnection();
+		
+		String sql = "update t_user set \r\n"
+				+ "pwd = ?, \r\n"
+				+ "name = ?, \r\n"
+				+ "email = ?,\r\n"
+				+ "birth = ?, \r\n"
+				+ "sns = ?, \r\n"
+				+ "reg_date = ? \r\n"
+				+ "where id = ?";
+//		String sql = "update t_user set values (?, ?, ?, ?, ?, ?, now())";
+//		String sql = "update t_user set name = ? where id = ?";
+		
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+//		pstmt.setString(1, user.getId());
+		pstmt.setString(1, user.getPwd());
+		pstmt.setString(2, user.getName());
+		pstmt.setString(3, user.getEmail());
+		pstmt.setDate(4, new java.sql.Date(user.getBirth().getTime()));
+		pstmt.setString(5, user.getSns());
+		pstmt.setDate(6, new java.sql.Date(0));
+		pstmt.setString(7,user.getId());
+		
+		int rowCnt = pstmt.executeUpdate();
+		return rowCnt;
 	}
 }
